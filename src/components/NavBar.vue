ea019deb84966ddca243413e32573f8b86b8c260
Kristoffer-Staugaard <kristofferstaugaard@gmail.com>
2025-03-18 15:35:39 +0100

    navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,91 @@
 <script setup>
-
 import NavBarButton from "./NavBarButton.vue";
+import ForsideIcon from "../assets/icons/gauge-solid-2.svg";
+import ForsideHoverIcon from "../assets/icons/gauge-solid.svg";
+import SkemaerIcon from "../assets/icons/calendar-check-solid-2.svg";
+import SkemaerHoverIcon from "../assets/icons/calendar-check-solid.svg";
+import ObjekterIcon from "../assets/icons/qrcode-solid-2.svg";
+import ObjekterHoverIcon from "../assets/icons/qrcode-solid.svg";
+import RapporterIcon from "../assets/icons/chart-line-solid-2.svg";
+import RapporterHoverIcon from "../assets/icons/chart-line-solid.svg";
+import DokumenterIcon from "../assets/icons/file-solid-2.svg";
+import DokumenterHoverIcon from "../assets/icons/file-solid.svg";
+import PlanlægningIcon from "../assets/icons/calendar-days-solid-2.svg";
+import PlanlægningHoverIcon from "../assets/icons/calendar-days-solid.svg";
+import BrugereIcon from "../assets/icons/user-group-solid-2.svg";
+import BrugereHoverIcon from "../assets/icons/user-group-solid.svg";
 </script>
 
 <template>
   <div class="nav-bar">
     <img src="../assets/icons/DBI_logo.svg" alt="logo" class="logo">
+    <nav>
+      <NavBarButton
+        :imgSrc="ForsideIcon"
+        :hoverImgSrc="ForsideHoverIcon"
+        imgAlt="forside icon"
+      >
+        <p>Forside</p>
+      </NavBarButton>
 
-    <NavBarButton>
-      <img src="../assets/icons/gauge-solid.svg" alt="navbar icon" class="button-icon">
-      <p>Forside</p>
-    </NavBarButton>
+      <NavBarButton
+        :imgSrc="SkemaerIcon"
+        :hoverImgSrc="SkemaerHoverIcon"
+        imgAlt="skemaer icon"
+      >
+        <p>Skemaer</p>
+      </NavBarButton>
+
+      <NavBarButton
+        :imgSrc="ObjekterIcon"
+        :hoverImgSrc="ObjekterHoverIcon"
+        imgAlt="objekter icon"
+      >
+        <p>Objekter</p>
+      </NavBarButton>
+
+      <NavBarButton
+        :imgSrc="RapporterIcon"
+        :hoverImgSrc="RapporterHoverIcon"
+        imgAlt="rapporter icon"
+      >
+        <p>Rapporter</p>
+      </NavBarButton>
+
+      <NavBarButton
+        :imgSrc="DokumenterIcon"
+        :hoverImgSrc="DokumenterHoverIcon"
+        imgAlt="dokumenter icon"
+      >
+        <p>Dokumenter</p>
+      </NavBarButton>
+
+      <NavBarButton
+        :imgSrc="PlanlægningIcon"
+        :hoverImgSrc="PlanlægningHoverIcon"
+        imgAlt="planlægning icon"
+      >
+        <p>Planlægning</p>
+      </NavBarButton>
+
+      <NavBarButton
+        :imgSrc="BrugereIcon"
+        :hoverImgSrc="BrugereHoverIcon"
+        imgAlt="brugere icon"
+      >
+        <p>Brugere</p>
+      </NavBarButton>
+    </nav>
   </div>
 </template>
 
 <style scoped lang="scss">
+
+nav {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
 
 .logo {
   width: 200px;
@@ -26,8 +97,8 @@ import NavBarButton from "./NavBarButton.vue";
 }
 
 .button-icon {
-  width: 20px;
-  height: 20px;
+  width: 24px;
+  height: 24px;
 }
 
 </style>
--- src/components/NavBarButton.vue
+++ src/components/NavBarButton.vue
@@ -1,31 +1,43 @@
 <script setup>
+import { ref } from "vue";
+
 defineProps({
   imgSrc: {
     type: String,
-    default: "../assets/icons/gauge-solid.svg"
+    required: true
+  },
+  hoverImgSrc: {
+    type: String,
+    required: true
   },
   imgAlt: {
     type: String,
     default: "icon"
   }
 });
+
+const isHovered = ref(false);
 </script>
 
 <template>
-  <button>
-    <slot>
-      <img :src="imgSrc" :alt="imgAlt" class="button-icon">
-      <p>Button</p>
-    </slot>
+  <button
+    @mouseenter="isHovered = true"
+    @mouseleave="isHovered = false"
+  >
+    <img
+      :src="isHovered ? hoverImgSrc : imgSrc"
+      :alt="imgAlt"
+      class="button-icon"
+    >
+    <slot></slot>
   </button>
 </template>
 
 <style scoped lang="scss">
-
 button {
   width: 100%;
   height: 70px;
-  background-color: #2B7393;
+  background-color: #F4F5F5;
   border: none;
   border-radius: 25px;
   padding: 0 28px;
@@ -35,7 +47,17 @@ button {
   gap: 24px;
   font-size: 25px;
   font-weight: 100;
-  color: #FFFFFF;
+  color: #707070;
+  cursor: pointer;
+}
 
+button:hover {
+  background-color: #A4CBE0;
+  color: #FFFFFF;
+}
+
+.button-icon {
+  width: 24px;
+  height: 24px;
 }
 </style>

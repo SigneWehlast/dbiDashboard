2b2a395b64553923ccd3afe5545d83353ccdc870
Kristoffer-Staugaard <kristofferstaugaard@gmail.com>
2025-03-18 13:55:15 +0100

    font

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,7 +8,7 @@ import NavBarButton from "./NavBarButton.vue";
     <img src="../assets/icons/DBI_logo.svg" alt="logo" class="logo">
 
     <NavBarButton>
-
+      <img src="../assets/icons/gauge-solid.svg" alt="navbar icon" class="button-icon">
       <p>Forside</p>
     </NavBarButton>
   </div>
@@ -23,6 +23,11 @@ import NavBarButton from "./NavBarButton.vue";
 .nav-bar {
   width: 300px;
   height: 100%;
+}
+
+.button-icon {
+  width: 20px;
+  height: 20px;
 }
 
 </style>
--- src/components/NavBarButton.vue
+++ src/components/NavBarButton.vue
@@ -1,10 +1,20 @@
 <script setup>
-
+defineProps({
+  imgSrc: {
+    type: String,
+    default: "../assets/icons/gauge-solid.svg"
+  },
+  imgAlt: {
+    type: String,
+    default: "icon"
+  }
+});
 </script>
 
 <template>
   <button>
     <slot>
+      <img :src="imgSrc" :alt="imgAlt" class="button-icon">
       <p>Button</p>
     </slot>
   </button>
@@ -17,8 +27,15 @@ button {
   height: 70px;
   background-color: #2B7393;
   border: none;
-  padding: 0;
+  border-radius: 25px;
+  padding: 0 28px;
   margin: 0;
-}
+  display: flex;
+  align-items: center;
+  gap: 24px;
+  font-size: 25px;
+  font-weight: 100;
+  color: #FFFFFF;
 
+}
 </style>

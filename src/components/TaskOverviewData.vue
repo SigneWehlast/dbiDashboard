6920be27854491b5f0b37ccc729187497d901cfa
khni48704 <hjerno.kasper@gmail.com>
2025-04-28 09:22:20 +0200

    Update TaskOverviewData.vue

--- src/components/TaskOverviewData.vue
+++ src/components/TaskOverviewData.vue
@@ -11,12 +11,12 @@ const props = defineProps({
 
 const tasks = ref([
   { title: "ABA anlæg (Månedlig egenkontrol) BR18/7.5.1.3", deadline: "23.03.2025", status: "Udført" },
-  { title: "ABA anlæg (Månedlig egenkontrol) BR18/7.5.1.3", deadline: "23.04.2025", status: "Udført" },
+  { title: "ABA anlæg (Månedlig egenkontrol) BR18/7.5.1.3", deadline: "28.04.2025", status: "Udført" },
   { title: "ABA Månedskontrol 6.1", deadline: "19.02.2025", status: "Overskredet" },
-  { title: "ABA Månedskontrol 6.1", deadline: "23.04.2025", status: "Overskredet" },
+  { title: "ABA Månedskontrol 6.1", deadline: "28.04.2025", status: "Overskredet" },
   { title: "ABV anlæg (Månedlig egenkontrol) BR18/7.5.8.1", deadline: "28.02.2025", status: "Overskredet" },
   { title: "Tjek ventilationssystem (Ugentlig rutine)", deadline: "30.04.2025", status: "Igangværende" },
-  { title: "Tjek ventilationssystem (Ugentlig rutine)", deadline: "23.04.2025", status: "Igangværende" }
+  { title: "Tjek ventilationssystem (Ugentlig rutine)", deadline: "28.04.2025", status: "Igangværende" }
 ]);
 
 function formatDate(date) {

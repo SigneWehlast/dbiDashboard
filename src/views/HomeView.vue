6a368fb12d3f9e2bf43872ae1d0838d006386d57
SigneWehlast <119939548+SigneWehlast@users.noreply.github.com>
2025-04-28 11:42:39 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,39 @@
 <script setup>
 import OverviewWidget from "../components/OverviewWidget.vue";
 import TaskOverview from "../components/TaskOverview.vue";
+
+import { ref, onMounted } from 'vue';
+import { onAuthStateChanged } from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
+import { auth, db } from '@/configs/firebase';
+
+const firstName = ref('');
+const lastName = ref('');
+
+onMounted(() => {
+  onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      const uid = user.uid;
+      const userDocRef = doc(db, 'users', uid);
+      const userDocSnap = await getDoc(userDocRef);
+
+      if (userDocSnap.exists()) {
+        const data = userDocSnap.data();
+        firstName.value = data.firstName || '';
+        lastName.value = data.lastName || '';
+      } else {
+        console.log('No such user document!');
+      }
+    } else {
+      console.log('User not logged in');
+    }
+  });
+});
 </script>
 
 <template>
+  <p class="p1">Velkommen tilbage,</p>
+  <h1>{{ firstName }} {{ lastName }}</h1>
   <div class="home-view__overview">
       <OverviewWidget />
       <TaskOverview />

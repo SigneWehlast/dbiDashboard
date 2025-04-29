a20495c84eb4237bd68c600338938fabd48707ad
khni48704 <hjerno.kasper@gmail.com>
2025-04-29 14:08:37 +0200

    Håndtering af bruger logik via pinia i stedet for i comp

--- src/components/ProfileBar.vue
+++ src/components/ProfileBar.vue
@@ -1,45 +1,21 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import { onAuthStateChanged } from 'firebase/auth';
-import { doc, getDoc } from 'firebase/firestore';
-import { auth, db } from '@/configs/firebase';
+import { useAuthStore } from '@/stores/AuthStore';
+import { storeToRefs } from 'pinia';
 import SearchBar from '@/components/SearchBar.vue';
 
-const firstName = ref('');
-const lastName = ref('');
-const companyName = ref('');
-
-onMounted(() => {
-  onAuthStateChanged(auth, async (user) => {
-    if (user) {
-      const uid = user.uid;
-      const userDocRef = doc(db, 'users', uid);
-      const userDocSnap = await getDoc(userDocRef);
-
-      if (userDocSnap.exists()) {
-        const data = userDocSnap.data();
-        firstName.value = data.firstName || '';
-        lastName.value = data.lastName || '';
-        companyName.value = data.companyName || '';
-      } else {
-        console.log('No such user document!');
-      }
-    } else {
-      console.log('User not logged in');
-    }
-  });
-});
+const authStore = useAuthStore();
+const { user } = storeToRefs(authStore);
 </script>
 
 <template>
   <div class="right-view">
     <div class="profile-bar">
-   <SearchBar />
+      <SearchBar />
       <div class="profile-bar__wrapper">
-        <img src="../assets/icons/bell-solid.svg" alt="profile icon" class="profile-bar__icon">
+        <img src="../assets/icons/bell-solid.svg" alt="profile icon" class="profile-bar__icon" />
         <div class="profile-bar__wrapper-name">
-          <h3>{{ firstName }} {{ lastName }}</h3>
-          <p class="profile-bar__wrapper-company">{{ companyName }}</p>
+          <h3>{{ user.firstName }} {{ user.lastName }}</h3>
+          <p class="profile-bar__wrapper-company">{{ user.companyName }}</p>
         </div>
       </div>
     </div>
